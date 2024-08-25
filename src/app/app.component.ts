ac5d9acffa1e6cddd4adba99ac5cdd1493f27fb1
amanat-kumar <amanatkr6@gmail.com>
2024-08-25 18:04:49 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ObjectiveComponent } from './objective/objective.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CoursesComponent } from './courses/courses.component';
+import { OthersComponent } from './others/others.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    ObjectiveComponent, 
+    EducationComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    CoursesComponent,
+    OthersComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
